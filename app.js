dd753ce23e9de875bb987c07f712b7485bcad76c
jayesh wadhe <jayeshwadhe08@gmai.com>
2024-05-30 20:18:43 +0530

    ssc

--- app.js
+++ app.js
@@ -15,33 +15,6 @@ app.get('/', (req, res) => {
     res.sendStatus(200)
   })
 
-app.post('/signup', async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-
-    try {
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ msg: 'User already exists' });
-        }
-
-        user = new User({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-        await user.save();
-        res.status(201).json({ msg: 'User registered successfully' });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-});
-
 // Database Connection
 const dbURI = 'mongodb://localhost:27017/auth-api';
 
